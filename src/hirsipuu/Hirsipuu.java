59bda57ab5cae5a6b2b4562b6b535fb1a777a9fd
pauli <pauli@pauli-IdeaPad-U330p>
2017-05-09 21:56:16 +0300

    Logiikka toimii

--- src/hirsipuu/Hirsipuu.java
+++ src/hirsipuu/Hirsipuu.java
@@ -5,16 +5,25 @@ public class Hirsipuu {
     public static void main(String[] args) {
 
         HirsipuuLogiikka log = new HirsipuuLogiikka("Kissa");
-
+        System.out.println(log.salattuSana());
+        
         log.arvaaKirjain("A");
+        System.out.println(log.salattuSana());
         log.arvaaKirjain("d"); // virhe
+        System.out.println(log.salattuSana());
         log.arvaaKirjain("s");
+        System.out.println(log.salattuSana());
         log.arvaaKirjain("f"); // virhe
-        log.arvaaKirjain("d"); // virhe
-        log.arvaaKirjain("e"); // virhe
-
+        System.out.println(log.salattuSana());
+        log.arvaaKirjain("k"); 
+        System.out.println(log.salattuSana());
+        log.arvaaKirjain("i");
+        System.out.println(log.salattuSana());
+        
+                
         System.out.println("Arvatut kirjaimet: " + log.arvatutKirjaimet());
         System.out.println("Virheiden lukumäärä: " + log.virheidenLukumaara());
+
     }
 
 }
--- src/hirsipuu/HirsipuuLogiikka.java
+++ src/hirsipuu/HirsipuuLogiikka.java
@@ -28,10 +28,10 @@ public class HirsipuuLogiikka {
         kirjain = kirjain.toUpperCase(); // iso kirjain
 
         // Tutkitaan, onko kirjainta vielä arvattu
-        if (this.arvatutKirjaimet.indexOf(kirjain) == -1) {
+        if (!this.arvatutKirjaimet.contains(kirjain)) {
             arvatutKirjaimet += kirjain;
 
-            if (this.sana.indexOf(kirjain) == -1) {
+            if (!this.sana.contains(kirjain)) {
                 this.virheidenLukumaara += 1;
             }
         }
@@ -39,7 +39,27 @@ public class HirsipuuLogiikka {
     }
 
     public String salattuSana() {
-        // tulostaa esim KISS_
-        return this.arvatutKirjaimet;
+        String salattuSana = ""; // Palautettava merkkijono
+        
+        // Muodostetaan taulukko arvattavan sanan merkeistä
+        char[] sanaTaulukko = this.sana.toCharArray();
+        
+        /* Tutkitaan merkki kerrallaan, onko arvattavassa sanassa oleva
+        kirjain arvattujen kirjainten listalla. Huom. arvattava sana käydään
+        järjestyksessä alusta lähtien läpi, jolloin merkkien lisäys 
+        toimii. */
+        
+        for (char i : sanaTaulukko) {
+            if (!this.arvatutKirjaimet.contains(i+"")) {
+                salattuSana += '_';
+            } else {
+                salattuSana += i;
+            }
+        }
+        if (!salattuSana.contains("_")) {
+            System.out.println("Hienoa, arvasit sanan!");
+        }
+        return salattuSana;
     }
+
 }
